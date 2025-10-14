a6858d28e6c36c5c59fdc3e2768bd31b43533dba
Sathira Pramudith <sathirapramudith1@gmail.com>
2025-10-14 18:37:23 +0530

    gallery, index, app

--- hotel-website/src/App.js
+++ hotel-website/src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { AppProvider } from './context/AppContext';
+import Header from './components/common/Header/Header';
+import Footer from './components/common/Footer/Footer';
+import Home from './pages/Home';
+import Rooms from './pages/Rooms';
+import Booking from './pages/Booking';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Gallery from './pages/Gallery';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppProvider>
+      <div className="App">
+        <Header />
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/rooms" element={<Rooms />} />
+            <Route path="/booking" element={<Booking />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/gallery" element={<Gallery />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </AppProvider>
   );
 }
 
